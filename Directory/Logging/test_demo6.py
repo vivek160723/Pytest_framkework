595b72d0dd9a39c178620b2975cec515dc8d75bd
vivek <vivekchaudhary160723@gmail.com>
2025-01-14 11:25:01 +0530

    first commit

---
+++ Directory/Logging/test_demo6.py
@@ -0,0 +1,20 @@
+import logging
+
+
+def demo_log():
+    logger = logging.getLogger(__name__)
+    fileHandler = logging.FileHandler('logfile.log')
+
+    formatter = logging.Formatter("%(asctime)s:%(levelname)s:%(name)s:%(message)s")
+    fileHandler.setFormatter(formatter)
+
+    logger.addHandler(fileHandler)
+
+    logger.setLevel(logging.DEBUG)
+    logger.debug("This is a debug message")
+    logger.info("This is an info message")
+    logger.warning("This is a warning message")
+    logger.error("This is an error message")
+    logger.critical("This is a critical message")
+
+demo_log()
